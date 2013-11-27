aeff3981747fe4ed9aef31e0582dfbc060062ba0
aav300 <aav300@lab12.cpt.haw-hamburg.de>
2013-11-27 11:00:38 +0100

    added libraries and properies

---
+++ src/src/buschler/chord/Main.java
@@ -0,0 +1,14 @@
+package buschler.chord;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		while(true){
+			System.out.println("Nothing to do!");
+		}
+	}
+
+}
